7dce6828d977a74244c7f6bccb2b1c53d24e75d8
AmanDeka <amanjyoti.deka2020@vitstudent.ac.in>
2022-04-12 23:31:36 +0530

    main pg

--- style_changer.py
+++ style_changer.py
@@ -1,13 +1,33 @@
 from PyQt5 import QtWidgets, uic
 import sys
+from styleformer import Styleformer
+import torch
+import warnings
+warnings.filterwarnings("ignore")
 
 
 class StyleChanger(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Ui, self).__init__()
+        super(StyleChanger, self).__init__()
         uic.loadUi('app.ui', self)
+        self.button.clicked.connect(self.show_style)
+        self.sf1 = Styleformer(style = 0)
+        self.sf2 = Styleformer(style = 1)  
         self.show()
+
+    
+    def show_style(self):
+        self.recommender.clear()
+        formal = self.sf1.transfer(self.textbox.toPlainText())
+        informal = self.sf2.transfer(self.textbox.toPlainText())
+        self.recommender.addItem("[Formal]"+formal)
+        self.recommender.addItem("[Informal]"+informal)
+ 
 
 app = QtWidgets.QApplication(sys.argv)
 window = StyleChanger()
 app.exec_()
+
+
+
+
